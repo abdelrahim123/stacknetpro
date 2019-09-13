43ceb1c95a10244cd20027d23ff1eddbbdf2159f
abdelrahim123 <48460743+abdelrahim123@users.noreply.github.com>
2019-09-13 02:27:45 +0200

    ff

--- js/e.js
+++ js/e.js
@@ -2,11 +2,12 @@ var d1 = document.getElementById('logInAppearance');
 var d2 = document.getElementById('hiddenTitle');
 var d3 = document.getElementById('hiddenLogIn');
 var d4 = document.getElementById('close');
-d1.onclick=function(){
-    d3.style.display="block";
-    d2.style.display="none";
-}
 d4.onclick=function(){
     d3.style.display="none";
     d2.style.display="block";
 }
+d1.onclick=function(){
+    d3.style.display="block";
+    d2.style.display="none";
+}
+
